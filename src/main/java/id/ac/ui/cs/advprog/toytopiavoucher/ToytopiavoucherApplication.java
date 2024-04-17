1114e4b39e2b93090c3f96f718e23a1de12d8657
Bimantoro <bimantoro.widyadana@ui.ac.id>
2024-04-17 19:01:09 +0700

    initializw

---
+++ src/main/java/id/ac/ui/cs/advprog/toytopiavoucher/ToytopiavoucherApplication.java
@@ -0,0 +1,13 @@
+package id.ac.ui.cs.advprog.toytopiavoucher;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ToytopiavoucherApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ToytopiavoucherApplication.class, args);
+    }
+
+}
---
+++ src/test/java/id/ac/ui/cs/advprog/toytopiavoucher/ToytopiavoucherApplicationTests.java
@@ -0,0 +1,13 @@
+package id.ac.ui.cs.advprog.toytopiavoucher;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ToytopiavoucherApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
