72d96cfa0a9c737a5fdfc1d42be5c7bfd758e83c
Bimantoro <bimantoro.widyadana@ui.ac.id>
2024-04-23 20:41:37 +0700

    [GREEN] add repository annotation

--- src/main/java/id/ac/ui/cs/advprog/toytopiavoucher/repository/VoucherRepository.java
+++ src/main/java/id/ac/ui/cs/advprog/toytopiavoucher/repository/VoucherRepository.java
@@ -1,10 +1,12 @@
 package id.ac.ui.cs.advprog.toytopiavoucher.repository;
 
 import id.ac.ui.cs.advprog.toytopiavoucher.model.Voucher;
+import org.springframework.stereotype.Repository;
 
 import java.util.ArrayList;
 import java.util.List;
 
+@Repository
 public class VoucherRepository {
     private List<Voucher> voucherData;
 
