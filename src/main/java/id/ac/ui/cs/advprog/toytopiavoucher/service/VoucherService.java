078039257c21e60570f0bc4ccfe2b398e88f9406
Bimantoro <bimantoro.widyadana@ui.ac.id>
2024-04-23 20:42:01 +0700

    [RED] make voucher service skeleton

--- src/main/java/id/ac/ui/cs/advprog/toytopiavoucher/service/VoucherService.java
+++ src/main/java/id/ac/ui/cs/advprog/toytopiavoucher/service/VoucherService.java
@@ -1,2 +1,13 @@
-package id.ac.ui.cs.advprog.toytopiavoucher.service;public interface VoucherService {
+package id.ac.ui.cs.advprog.toytopiavoucher.service;
+
+import id.ac.ui.cs.advprog.toytopiavoucher.model.Voucher;
+
+import java.util.List;
+
+public interface VoucherService {
+    Voucher create(Voucher voucher);
+    Voucher edit(Voucher voucher);
+    Voucher delete(Voucher voucher);
+    Voucher findByCode(String code);
+    List<Voucher> findAll();
 }
--- src/main/java/id/ac/ui/cs/advprog/toytopiavoucher/service/VoucherServiceImpl.java
+++ src/main/java/id/ac/ui/cs/advprog/toytopiavoucher/service/VoucherServiceImpl.java
@@ -1,2 +1,43 @@
-package id.ac.ui.cs.advprog.toytopiavoucher.service;public class VoucherServiceImpl {
+package id.ac.ui.cs.advprog.toytopiavoucher.service;
+
+import id.ac.ui.cs.advprog.toytopiavoucher.model.Voucher;
+import id.ac.ui.cs.advprog.toytopiavoucher.repository.VoucherRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class VoucherServiceImpl implements VoucherService {
+    private VoucherRepository voucherRepository;
+
+    @Autowired
+    public VoucherServiceImpl(VoucherRepository voucherRepository) {
+        this.voucherRepository = voucherRepository;
+    }
+
+    @Override
+    public Voucher create(Voucher voucher) {
+        return null;
+    }
+
+    @Override
+    public Voucher edit(Voucher voucher) {
+        return null;
+    }
+
+    @Override
+    public Voucher delete(Voucher voucher) {
+        return null;
+    }
+
+    @Override
+    public Voucher findByCode(String code) {
+        return null;
+    }
+
+    @Override
+    public List<Voucher> findAll() {
+        return null;
+    }
 }
