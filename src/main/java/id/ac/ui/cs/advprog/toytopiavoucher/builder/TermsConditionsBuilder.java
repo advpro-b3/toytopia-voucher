a1a782125937f78930a48d89e31ecc3849211532
Bimantoro <bimantoro.widyadana@ui.ac.id>
2024-04-24 00:04:35 +0700

    add terms and conditions builder

--- src/main/java/id/ac/ui/cs/advprog/toytopiavoucher/builder/TermsConditionsBuilder.java
+++ src/main/java/id/ac/ui/cs/advprog/toytopiavoucher/builder/TermsConditionsBuilder.java
@@ -1,2 +1,30 @@
-package id.ac.ui.cs.advprog.toytopiavoucher.builder;public class TermsConditionsBuilder {
+package id.ac.ui.cs.advprog.toytopiavoucher.builder;
+
+import id.ac.ui.cs.advprog.toytopiavoucher.enums.PaymentMethod;
+import id.ac.ui.cs.advprog.toytopiavoucher.model.TermsConditions;
+
+public class TermsConditionsBuilder {
+    private TermsConditions termsConditions;
+
+    public TermsConditionsBuilder() {
+        this.reset();
+    }
+
+    public void reset() {
+        this.termsConditions = new TermsConditions();
+    }
+
+    public void setMinimumPurchase(double amount) {
+        termsConditions.setMinimumPurchase(amount);
+    }
+
+    public void setPaymentMethod(PaymentMethod method) {
+        termsConditions.setPaymentMethod(method);
+    }
+
+    public TermsConditions build() {
+        TermsConditions result = this.termsConditions;
+        this.reset();
+        return result;
+    }
 }
