947c04f3a4642a55bcba5d28606ca0aa9da0231b
Bimantoro <bimantoro.widyadana@ui.ac.id>
2024-05-08 19:30:01 +0700

    [RED] add tests for voucher after jpa

--- src/test/java/id/ac/ui/cs/advprog/toytopiavoucher/model/VoucherTest.java
+++ src/test/java/id/ac/ui/cs/advprog/toytopiavoucher/model/VoucherTest.java
@@ -1,58 +1,91 @@
 package id.ac.ui.cs.advprog.toytopiavoucher.model;
 
 import id.ac.ui.cs.advprog.toytopiavoucher.enums.PaymentMethod;
+import jakarta.persistence.Column;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+
+import java.time.LocalDate;
+import java.util.UUID;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 public class VoucherTest {
-    private String code;
+    private UUID code;
     private double discount;
-    private TermsConditions tc;
+    private Double maxDiscount;
+    private Double minPurchase;
+    private PaymentMethod paymentMethod;
+    private LocalDate creationDate;
+    private LocalDate expiryDate;
 
     @BeforeEach
     void setUp() {
-        code = "SHRIGMA-MALE-123";
+        code = UUID.randomUUID();
         discount = 0.5;
-        tc = new TermsConditions(50_000.0, PaymentMethod.ANY);
+        maxDiscount = 50_000.0;
+        minPurchase = 100_000.0;
+        paymentMethod = PaymentMethod.CREDIT_CARD;
+        creationDate = LocalDate.now();
+        expiryDate = creationDate.plusMonths(1);
+    }
+
+    @Test
+    void testCreateEmptyVoucher() {
+        Voucher v = new Voucher();
+        assertNull(v.getCode());
+        assertNotNull(v.getCreationDate());
+        assertNotNull(v.getPaymentMethod());
     }
 
     @Test
     void testCreateValidVoucher() {
-        Voucher v = new Voucher(code, discount, tc);
-        assertEquals(code, v.getCode());
+        Voucher v = new Voucher(discount, maxDiscount, minPurchase, paymentMethod, creationDate, expiryDate);
+        assertNull(v.getCode());
         assertEquals(discount, v.getDiscount());
-        assertEquals(tc, v.getTermsConditions());
+        assertEquals(maxDiscount, v.getMaxDiscount());
+        assertEquals(minPurchase, v.getMinPurchase());
+        assertEquals(paymentMethod, v.getPaymentMethod());
+        assertEquals(creationDate, v.getCreationDate());
+        assertEquals(expiryDate, v.getExpiryDate());
     }
 
     @Test
-    void testCreateWithNoTc() {
-        Voucher v = new Voucher(code, discount);
-        TermsConditions emptyTc = new TermsConditions();
-        assertEquals(emptyTc, v.getTermsConditions());
+    void testCreateInvalidDiscount() {
+        assertThrows(IllegalArgumentException.class,
+                () -> new Voucher(0.0, maxDiscount, minPurchase, paymentMethod, creationDate, expiryDate));
+        assertThrows(IllegalArgumentException.class,
+                () -> new Voucher(1.1, maxDiscount, minPurchase, paymentMethod, creationDate, expiryDate));
+        assertThrows(IllegalArgumentException.class,
+                () -> new Voucher(-1.0, maxDiscount, minPurchase, paymentMethod, creationDate, expiryDate));
     }
 
     @Test
-    void testCreateWithEmptyOrNullCode() {
-        code = "";
+    void testInvalidMaxDiscount() {
         assertThrows(IllegalArgumentException.class,
-                () -> new Voucher(code, discount, tc));
-        assertThrows(IllegalArgumentException.class,
-                () -> new Voucher(null, discount, tc));
+                () -> new Voucher(discount, -1.0, minPurchase, paymentMethod, creationDate, expiryDate));
     }
 
     @Test
-    void testCreateWithZeroOrLessDiscount() {
+    void testInvalidMinPurchase() {
         assertThrows(IllegalArgumentException.class,
-                () -> new Voucher(code, 0.0, tc));
-        assertThrows(IllegalArgumentException.class,
-                () -> new Voucher(code, -1.0, tc));
+                () -> new Voucher(discount, maxDiscount, -1.0, paymentMethod, creationDate, expiryDate));
     }
 
     @Test
-    void testCreateWithGreaterThanOneDiscount() {
+    void testInvalidExpiryDate() {
+        expiryDate = creationDate.minusDays(1);
         assertThrows(IllegalArgumentException.class,
-                () -> new Voucher(code, 1.1, tc));
+                () -> new Voucher(discount, maxDiscount, minPurchase, paymentMethod, creationDate, expiryDate));
+    }
+
+    @Test
+    void testNullArguments() {
+        assertThrows(IllegalArgumentException.class,
+                () -> new Voucher(null, maxDiscount, minPurchase, paymentMethod, creationDate, expiryDate));
+        assertThrows(IllegalArgumentException.class,
+                () -> new Voucher(discount, maxDiscount, minPurchase, null, creationDate, expiryDate));
+        assertThrows(IllegalArgumentException.class,
+                () -> new Voucher(discount, maxDiscount, minPurchase, paymentMethod, null, expiryDate));
     }
 }
