0013d3cd08b8ce1aa2a9619cc653572c8c68b900
Bimantoro <bimantoro.widyadana@ui.ac.id>
2024-05-26 16:11:47 +0700

    fix tests again

--- src/test/java/id/ac/ui/cs/advprog/toytopiavoucher/controller/VoucherControllerTest.java
+++ src/test/java/id/ac/ui/cs/advprog/toytopiavoucher/controller/VoucherControllerTest.java
@@ -172,6 +172,7 @@ public class VoucherControllerTest {
 
     @Test
     void editShouldReturnBadRequestIfCodeNull() throws Exception {
+        when(userService.isAdmin(token)).thenReturn(CompletableFuture.completedFuture(true));
         String jsonEdit = """
                 {
                     "code": null,
@@ -187,14 +188,20 @@ public class VoucherControllerTest {
         MockHttpServletRequestBuilder requestBuilder = put("/voucher/edit")
                 .contentType(MediaType.APPLICATION_JSON)
                 .accept(MediaType.APPLICATION_JSON)
+                .header("Authorization", token)
                 .content(jsonEdit);
 
-        this.mockMvc.perform(requestBuilder)
+        MvcResult mvcResult = this.mockMvc.perform(requestBuilder)
+                .andExpect(request().asyncStarted())
+                .andReturn();
+
+        this.mockMvc.perform(asyncDispatch(mvcResult))
                 .andExpect(status().isBadRequest());
     }
 
     @Test
     void editShouldReturnBadRequestIfDiscountNull() throws Exception {
+        when(userService.isAdmin(token)).thenReturn(CompletableFuture.completedFuture(true));
         String jsonEdit = """
                 {
                     "code": "%s",
@@ -210,14 +217,20 @@ public class VoucherControllerTest {
         MockHttpServletRequestBuilder requestBuilder = put("/voucher/edit")
                 .contentType(MediaType.APPLICATION_JSON)
                 .accept(MediaType.APPLICATION_JSON)
+                .header("Authorization", token)
                 .content(jsonEdit);
 
-        this.mockMvc.perform(requestBuilder)
+        MvcResult mvcResult = this.mockMvc.perform(requestBuilder)
+                .andExpect(request().asyncStarted())
+                .andReturn();
+
+        this.mockMvc.perform(asyncDispatch(mvcResult))
                 .andExpect(status().isBadRequest());
     }
 
     @Test
     void editShouldReturnBadRequestIfCreationDateNull() throws Exception {
+        when(userService.isAdmin(token)).thenReturn(CompletableFuture.completedFuture(true));
         String jsonEdit = """
                 {
                     "code": "%s",
@@ -233,13 +246,19 @@ public class VoucherControllerTest {
         MockHttpServletRequestBuilder requestBuilder = put("/voucher/edit")
                 .contentType(MediaType.APPLICATION_JSON)
                 .accept(MediaType.APPLICATION_JSON)
+                .header("Authorization", token)
                 .content(jsonEdit);
 
-        this.mockMvc.perform(requestBuilder)
+        MvcResult mvcResult = this.mockMvc.perform(requestBuilder)
+                .andExpect(request().asyncStarted())
+                .andReturn();
+
+        this.mockMvc.perform(asyncDispatch(mvcResult))
                 .andExpect(status().isBadRequest());
     }
     @Test
     void editShouldReturnBadRequestIfPaymentMethodNull() throws Exception {
+        when(userService.isAdmin(token)).thenReturn(CompletableFuture.completedFuture(true));
         String jsonEdit = """
                 {
                     "code": "%s",
@@ -255,13 +274,19 @@ public class VoucherControllerTest {
         MockHttpServletRequestBuilder requestBuilder = put("/voucher/edit")
                 .contentType(MediaType.APPLICATION_JSON)
                 .accept(MediaType.APPLICATION_JSON)
+                .header("Authorization", token)
                 .content(jsonEdit);
 
-        this.mockMvc.perform(requestBuilder)
+        MvcResult mvcResult = this.mockMvc.perform(requestBuilder)
+                .andExpect(request().asyncStarted())
+                .andReturn();
+
+        this.mockMvc.perform(asyncDispatch(mvcResult))
                 .andExpect(status().isBadRequest());
     }
     @Test
     void editShouldReturnBadRequestIfCodeNotFound() throws Exception {
+        when(userService.isAdmin(token)).thenReturn(CompletableFuture.completedFuture(true));
         String jsonEdit = """
                 {
                     "code": "%s",
@@ -279,9 +304,14 @@ public class VoucherControllerTest {
         MockHttpServletRequestBuilder requestBuilder = put("/voucher/edit")
                 .contentType(MediaType.APPLICATION_JSON)
                 .accept(MediaType.APPLICATION_JSON)
+                .header("Authorization", token)
                 .content(jsonEdit);
 
-        this.mockMvc.perform(requestBuilder)
+        MvcResult mvcResult = this.mockMvc.perform(requestBuilder)
+                .andExpect(request().asyncStarted())
+                .andReturn();
+
+        this.mockMvc.perform(asyncDispatch(mvcResult))
                 .andExpect(status().isBadRequest());
     }
 
@@ -329,5 +359,125 @@ public class VoucherControllerTest {
 
         this.mockMvc.perform(asyncDispatch(mvcResult))
                 .andExpect(status().isNotFound());
+    }
+
+    @Test
+    void shouldReturnBadRequestIfInvalidToken() throws Exception {
+        when(userService.isAdmin(token)).thenReturn(CompletableFuture.failedFuture(new Exception()));
+        when(voucherService.findByCode(code)).thenReturn(CompletableFuture.completedFuture(voucher));
+        String jsonEdit = """
+                {
+                    "code": "%s",
+                    "discount": 0.5,
+                    "maxDiscount": null,
+                    "minPurchase": 100000.0,
+                    "paymentMethod": "BANK_TRANSFER",
+                    "creationDate": "%s",
+                    "expiryDate": null
+                }""";
+        jsonEdit = String.format(jsonEdit, code, LocalDate.now().format(DateTimeFormatter.ofPattern("yyyy-MM-dd")));
+
+        MockHttpServletRequestBuilder createRequest = post("/voucher/create")
+                .contentType(MediaType.APPLICATION_JSON)
+                .accept(MediaType.APPLICATION_JSON)
+                .header("Authorization", token)
+                .content(json);
+
+        MockHttpServletRequestBuilder editRequest = put("/voucher/edit")
+                .contentType(MediaType.APPLICATION_JSON)
+                .accept(MediaType.APPLICATION_JSON)
+                .header("Authorization", token)
+                .content(jsonEdit);
+
+        MockHttpServletRequestBuilder deleteRequest = delete(String.format("/voucher/delete/%s", UUID.randomUUID()))
+                .contentType(MediaType.APPLICATION_JSON)
+                .accept(MediaType.APPLICATION_JSON)
+                .header("Authorization", token);
+
+        MockHttpServletRequestBuilder deleteAllRequest = delete("/voucher/deleteAll")
+                .contentType(MediaType.APPLICATION_JSON)
+                .accept(MediaType.APPLICATION_JSON)
+                .header("Authorization", token);
+
+        MvcResult createResult = this.mockMvc.perform(createRequest)
+                .andExpect(request().asyncStarted())
+                .andReturn();
+
+        MvcResult editResult = this.mockMvc.perform(editRequest)
+                .andExpect(request().asyncStarted())
+                .andReturn();
+
+        MvcResult deleteResult = this.mockMvc.perform(deleteRequest)
+                .andExpect(request().asyncStarted())
+                .andReturn();
+
+        MvcResult deleteAllResult = this.mockMvc.perform(deleteAllRequest)
+                .andExpect(request().asyncStarted())
+                .andReturn();
+
+        this.mockMvc.perform(asyncDispatch(createResult)).andExpect(status().isBadRequest());
+        this.mockMvc.perform(asyncDispatch(editResult)).andExpect(status().isBadRequest());
+        this.mockMvc.perform(asyncDispatch(deleteResult)).andExpect(status().isBadRequest());
+        this.mockMvc.perform(asyncDispatch(deleteAllResult)).andExpect(status().isBadRequest());
+    }
+
+    @Test
+    void shouldReturnForbiddenIfNotAdmin() throws Exception {
+        when(userService.isAdmin(token)).thenReturn(CompletableFuture.completedFuture(false));
+        when(voucherService.findByCode(code)).thenReturn(CompletableFuture.completedFuture(voucher));
+        String jsonEdit = """
+                {
+                    "code": "%s",
+                    "discount": 0.5,
+                    "maxDiscount": null,
+                    "minPurchase": 100000.0,
+                    "paymentMethod": "BANK_TRANSFER",
+                    "creationDate": "%s",
+                    "expiryDate": null
+                }""";
+        jsonEdit = String.format(jsonEdit, code, LocalDate.now().format(DateTimeFormatter.ofPattern("yyyy-MM-dd")));
+
+        MockHttpServletRequestBuilder createRequest = post("/voucher/create")
+                .contentType(MediaType.APPLICATION_JSON)
+                .accept(MediaType.APPLICATION_JSON)
+                .header("Authorization", token)
+                .content(json);
+
+        MockHttpServletRequestBuilder editRequest = put("/voucher/edit")
+                .contentType(MediaType.APPLICATION_JSON)
+                .accept(MediaType.APPLICATION_JSON)
+                .header("Authorization", token)
+                .content(jsonEdit);
+
+        MockHttpServletRequestBuilder deleteRequest = delete(String.format("/voucher/delete/%s", UUID.randomUUID()))
+                .contentType(MediaType.APPLICATION_JSON)
+                .accept(MediaType.APPLICATION_JSON)
+                .header("Authorization", token);
+
+        MockHttpServletRequestBuilder deleteAllRequest = delete("/voucher/deleteAll")
+                .contentType(MediaType.APPLICATION_JSON)
+                .accept(MediaType.APPLICATION_JSON)
+                .header("Authorization", token);
+
+        MvcResult createResult = this.mockMvc.perform(createRequest)
+                .andExpect(request().asyncStarted())
+                .andReturn();
+
+        MvcResult editResult = this.mockMvc.perform(editRequest)
+                .andExpect(request().asyncStarted())
+                .andReturn();
+
+        MvcResult deleteResult = this.mockMvc.perform(deleteRequest)
+                .andExpect(request().asyncStarted())
+                .andReturn();
+
+        MvcResult deleteAllResult = this.mockMvc.perform(deleteAllRequest)
+                .andExpect(request().asyncStarted())
+                .andReturn();
+
+        this.mockMvc.perform(asyncDispatch(createResult)).andExpect(status().isForbidden());
+        this.mockMvc.perform(asyncDispatch(editResult)).andExpect(status().isForbidden());
+        this.mockMvc.perform(asyncDispatch(deleteResult)).andExpect(status().isForbidden());
+        this.mockMvc.perform(asyncDispatch(deleteAllResult)).andExpect(status().isForbidden());
     }
 }
